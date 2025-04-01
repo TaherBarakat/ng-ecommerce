9cd0b921ca64b84b9e3a648133d37f6a9bd68a90
Taher Barakat <taher.barakat12223@gmail.com>
2025-04-01 22:15:57 +0300

    stop angular from creating .spec files init the navbar and footer components

---
+++ src/app/shared/components/navbear/navbear.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbear',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbear.component.html',
+  styleUrl: './navbear.component.css'
+})
+export class NavbearComponent {
+
+}
