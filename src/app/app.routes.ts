44cfcd4cd1c4206af89bb2da605bb4636e4bb364
Taher Barakat <taher.barakat12223@gmail.com>
2025-04-03 18:59:31 +0300

    modify the app config ro take a # for the route and add fake routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: '', component: HomePageComponent },
-  { path: '', component: HomePageComponent },
-  { path: '', component: HomePageComponent },
+  { path: 'explore', component: HomePageComponent },
+  { path: 'projects', component: HomePageComponent },
+  { path: 'about-us', component: HomePageComponent },
 ];
